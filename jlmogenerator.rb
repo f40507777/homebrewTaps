81a9ca2cebddfeda4408613ccd3c61128ff1c99e
finnhe <finn.he@jkopay.com>
2018-11-19 15:43:08 +0800

    remove sha

--- jlmogenerator.rb
+++ jlmogenerator.rb
@@ -2,7 +2,6 @@
   desc ""
   homepage ""
   url "https://github.com/f40507777/JLMogenerator/archive/1.3.tar.gz"
-  sha256 "149eeb602e0d6731c4baa891e7a967edf35edbbe18381736c32967f5bd489492"
 
   def install
     xcodebuild "-target", "jlmogenerator", "-configuration", "Release", "SYMROOT=symroot", "OBJROOT=objroot"
